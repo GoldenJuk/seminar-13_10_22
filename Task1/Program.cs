70a8bd61442c072e2f927a68c3eddf6141701386
Alex Kizilov <as_k@list.ru>
2022-10-14 07:40:03 +0400

    Add Task1 - square_number

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Программа 
+считает квадрат целого числа
+*/
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrnumber = number * number;
+
+Console.WriteLine("Квадрат Вашего числа = " + sqrnumber);
+
+// Можно сделать так
+
+Console.WriteLine($"Квадрат Вашего числа = {sqrnumber}");
