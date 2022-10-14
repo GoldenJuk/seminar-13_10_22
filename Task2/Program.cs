4fd41a926c54d35bcf77596540acf6d31b280146
Alex Kizilov <as_k@list.ru>
2022-10-14 07:53:03 +0400

    Task2__Is the first number of the square second?

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Является ли первое число квадратом второго
+
+Console.WriteLine("Введите число1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int sqrnumber1 = number1 * number1;
+
+if (number2 == sqrnumber1)
+{
+    Console.WriteLine("Первое число является квадратом Вашего второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом Вашего второго числа");
+}
