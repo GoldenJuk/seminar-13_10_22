44133fdeef7964405d1303a6471a46eb44fab54d
Alex Kizilov <as_k@list.ru>
2022-10-14 11:16:24 +0400

    Task3 Changes 10-15

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,35 +6,41 @@ Console.WriteLine("");
 Console.Write("Введите номер дня недели (от 1 до 7) ");
 
 int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 7 || number < 1)
+{
+    Console.WriteLine("Дней недели всего 7, введите число от 1 до 7 ");
+}
+
+else
+{
+
 if (number == 1) 
 {
-Console.WriteLine("Это понедельник");
+    Console.WriteLine("Это понедельник");
 }
 if (number == 2) 
 {
-Console.WriteLine("Это вторник");
+    Console.WriteLine("Это вторник");
 }
 if (number == 3) 
 {
-Console.WriteLine("Это среда");
+    Console.WriteLine("Это среда");
 }
 if (number == 4) 
 {
-Console.WriteLine("Это четверг");
+    Console.WriteLine("Это четверг");
 }
 if (number == 5) 
 {
-Console.WriteLine("Это пятница");
+    Console.WriteLine("Это пятница");
 }
 if (number == 6) 
 {
-Console.WriteLine("Это суббота");
+    Console.WriteLine("Это суббота");
 }
 if (number == 7) 
 {
-Console.WriteLine("Это воскресенье");
+    Console.WriteLine("Это воскресенье");
 }
-if (number > 7) 
-{
-Console.WriteLine("Введите число от 1 до 7");
 }
