740c4de855b5ecda4c7280d4ffdf373231d3cbb6
Alex Kizilov <as_k@list.ru>
2022-10-14 16:29:19 +0400

    Task4 The end

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿/*Программа, которая на вход принимает 
+одно число (N), а на выходе показывает все целые 
+числа в промежутке от -N до N.
+*/
+
+Console.WriteLine("ВВедите любое число ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int negativN = N * (-1);
+while (negativN <= N) 
+{
+    Console.Write(negativN); Console.Write("   ");
+    negativN++;
+}
