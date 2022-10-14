da56bce403944b0b3f6d855049fc8509d3c6a536
Alex Kizilov <as_k@list.ru>
2022-10-14 07:46:11 +0400

    Task1 - The end

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,12 +2,15 @@
 считает квадрат целого числа
 */
 
+Console.WriteLine("");
 Console.Write("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
 
 int sqrnumber = number * number;
 
 Console.WriteLine("Квадрат Вашего числа = " + sqrnumber);
+Console.WriteLine("");
 
 /*Можно сделать так
 
