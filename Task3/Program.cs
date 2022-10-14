3e8445ca7b9f5508d411bb377a5767d799616f3f
Alex Kizilov <as_k@list.ru>
2022-10-14 10:05:40 +0400

    Task3 The end

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿/* Программу, которая будет выдавать 
+название дня недели по заданному номеру;
+*/
+
+Console.WriteLine("");
+Console.Write("Введите номер дня недели (от 1 до 7) ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1) 
+{
+Console.WriteLine("Это понедельник");
+}
+if (number == 2) 
+{
+Console.WriteLine("Это вторник");
+}
+if (number == 3) 
+{
+Console.WriteLine("Это среда");
+}
+if (number == 4) 
+{
+Console.WriteLine("Это четверг");
+}
+if (number == 5) 
+{
+Console.WriteLine("Это пятница");
+}
+if (number == 6) 
+{
+Console.WriteLine("Это суббота");
+}
+if (number == 7) 
+{
+Console.WriteLine("Это воскресенье");
+}
+if (number > 7) 
+{
+Console.WriteLine("Введите число от 1 до 7");
+}
